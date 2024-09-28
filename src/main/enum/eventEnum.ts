071fc69e63ee52a5a88216657b42b216308181b1
brokestar233 <161171586+brokestar233@users.noreply.github.com>
2024-09-28 23:16:36 +0800

    Initial commit

---
+++ src/main/enum/eventEnum.ts
@@ -0,0 +1,26 @@
+export const enum EventEnum {
+  //----------------- * Wrapper 部分 * -----------------
+
+  // QQ的日志
+  sendLog = 'NodeIQQNTWrapperSession/create/getNodeMiscService/sendLog',
+
+  // 登录成功，能拿到 uin,uid
+  onQRCodeLoginSucceed = 'NodeIKernelLoginService/get/addKernelLoginListener/onQRCodeLoginSucceed',
+
+  // 收到新消息
+  onRecvMsg = 'NodeIQQNTWrapperSession/create/getMsgService/addKernelMsgListener/onRecvMsg',
+
+  // 发送消息
+  sendMsg = 'NodeIQQNTWrapperSession/create/getMsgService/sendMsg',
+
+  // 广告?
+  requestTianshuAdv = 'NodeIQQNTWrapperSession/create/getMsgService/requestTianshuAdv',
+
+  // 更新
+  fetchUnitedCommendConfig = 'NodeIQQNTWrapperSession/create/getUnitedConfigService/fetchUnitedCommendConfig',
+
+  //----------------- * IPC 部分 * -----------------
+
+  // 收到新消息(只会收到已激活窗口消息)
+  onRecvActiveMsg = 'nodeIKernelMsgListener/onRecvActiveMsg'
+}
