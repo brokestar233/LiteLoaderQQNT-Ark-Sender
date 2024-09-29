03f1bb49ab6067fb1151e9d8db40c8a34ab85135
brokestar233 <161171586+brokestar233@users.noreply.github.com>
2024-09-30 01:08:41 +0800

    Delete out directory

--- out/preload/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";const r=require("electron"),n="ark_sender";r.contextBridge.exposeInMainWorld(n,{configUpdate:e=>r.ipcRenderer.invoke(`LiteLoader.${n}.configUpdate`,e),onBarClick:(e,i)=>r.ipcRenderer.invoke(`LiteLoader.${n}.onBarClick`,e,i)});
